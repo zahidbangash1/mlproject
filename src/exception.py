1ff47d9fabb850eea724f3127c75a82cd3de296e
Zahid Bangash <zahidbangash@gmail.com>
2026-02-18 02:19:22 +0500

    data ingestion

--- src/exception.py
+++ src/exception.py
@@ -1,12 +1,13 @@
 import sys
 import logging
+from logger import logging
 
 
 def error_message_detail(error,error_detail:sys):
    _,_,exc_tb = error_detail.exc_info()
    file_name = exc_tb.tb_frame.f_code.co_filename
-   error_message = "Error occur in python scripth name [{0}] line number [{1}] error message [{2}]".format(
-      file_name,exc_tb.tb_lineno.str(error))
+   error_message = "Error occured in python script name [{0}] line number [{1}] error message [{2}]".format(
+      file_name,exc_tb.tb_lineno,str(error))
 
    return error_message
 
@@ -17,12 +18,17 @@ class CustomException(Exception):
       super().__init__(error_message)
       self.error_message=error_message_detail(error_message,error_detail=error_detail)
 
-   def __init__(self):
+   def __str__(self):
       return self.error_message
       
 
 
-      
+if __name__=="__main__":
+   try:
+      a=1/0
+   except Exception as e:
+      logging.info("Divide by zero")
+      raise CustomException(e,sys)
 
 
    
